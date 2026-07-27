fc8744cf1cf3bae9a5ad9b17f9c2edca07917c86
arim <ahrim0813@naver.com>
2026-07-27 14:18:56 +0900

    react-beginner 삭제 및 create-react-app 폴더 정상화

---
+++ 03-create-react-app/src/routes/Detail.js
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `https://movies-api.accel.li/api/v2/movie_details.json?movie_id=${id}`,
+      )
+    ).json();
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return <h1>Detail</h1>;
+}
+
+export default Detail;
