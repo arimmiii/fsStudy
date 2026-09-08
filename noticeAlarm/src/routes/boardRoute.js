62f0866c3107c2f7125f12259611ec3ef69fd373
arimmiii <ahrim5558@gmail.com>
2026-09-08 16:26:43 +0900

    noticeAlarm 분리 및 제거

--- noticeAlarm/src/routes/boardRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const boardController = require("../controllers/boardController");
-const authMiddleware = require("../middlewares/authMiddleware");
-
-router.post("/", authMiddleware, boardController.updateBoards);
-router.get("/", authMiddleware, boardController.getBoards);
-module.exports = router;
